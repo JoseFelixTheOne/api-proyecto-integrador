538f5b30ff4021b8a748808d1fbf0bc4a071ddfb
elifio <elifioroldan@hotmail.com>
2023-10-17 23:17:04 -0500

    Cambios Iniciales

--- src/main/java/com/cibertec/proyecto/integrador/services/LoginService.java
+++ src/main/java/com/cibertec/proyecto/integrador/services/LoginService.java
@@ -3,7 +3,9 @@ package com.cibertec.proyecto.integrador.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 @Service
 public class LoginService {
 
@@ -14,9 +16,38 @@ public class LoginService {
         this.jdbcTemplate=jdbcTemplate;
     }
 
-    public int login() {
-        String consultaSql = "SELECT COUNT(*) FROM tu_tabla";
+    public int login(String usuario,String contra) {
+        String cifrado= cifrarCadena(contra);
+        String consultaSql = "SELECT COUNT(*) FROM users where username='"+usuario+"' and password='"+cifrado+"'";
         int contador = jdbcTemplate.queryForObject(consultaSql, Integer.class);
         return contador;
     }
+
+    /*
+
+        insert into roles(name)  values('Admin');
+
+        insert into users(id,username,email,password,document,role)
+        values(1,'eroldan','elifioroldan@hotmail.com',
+        'EF797C8118F02DFB649607DD5D3F8C7623048C9C063D532CC95C5ED7A898A64F','72722415',1)
+
+    * */
+    public static String cifrarCadena(String cadena) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] bytecadena = cadena.getBytes(StandardCharsets.UTF_8);
+            byte[] bytecifrado = digest.digest(bytecadena);
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : bytecifrado) {
+                String hex = String.format("%02X", b);
+                hexString.append(hex);
+            }
+
+            return hexString.toString().toLowerCase();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
