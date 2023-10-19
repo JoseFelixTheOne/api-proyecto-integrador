cbf4bca50fab1efbf9fe9a9b58490bc5ab85fc78
IRTP\eroldan <eroldan@irtp.gob.pe>
2023-10-19 16:24:22 -0500

    Merge remote-tracking branch 'origin/feature/login' into feature/login

--- src/main/java/com/cibertec/proyecto/integrador/controller/UsuarioController.java
+++ src/main/java/com/cibertec/proyecto/integrador/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.cibertec.proyecto.integrador.controller;
 
+import com.cibertec.proyecto.integrador.config.JWTProvider;
 import com.cibertec.proyecto.integrador.entity.Usuario;
 import com.cibertec.proyecto.integrador.services.LoginService;
 import com.cibertec.proyecto.integrador.services.UsuarioService;
@@ -7,29 +8,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class UsuarioController {
+    private final JWTProvider jwtProvider;
 
     private final UsuarioService servicio;
     @Autowired
-    public UsuarioController(UsuarioService servicio ) {
+    public UsuarioController(UsuarioService servicio, JWTProvider jwtProvider  ) {
         this.servicio=servicio;
+        this.jwtProvider=jwtProvider;
     }
 
     @PostMapping("/usuario")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<String> crearUsuario(@RequestBody Usuario usuario) {
-        int filasAfectadas = servicio.insertarUsuario(usuario);
-        if (filasAfectadas == 1) {
-            return ResponseEntity.status(HttpStatus.CREATED).body("Usuario creado exitosamente");
+    public ResponseEntity<String> crearUsuario(@RequestBody Usuario usuario ,  @RequestHeader("Authorization") String token) {
+        if (jwtProvider.validateToken(token.replace("Bearer ", ""))) {
+            int filasAfectadas = servicio.insertarUsuario(usuario);
+            if (filasAfectadas == 1) {
+                return ResponseEntity.status(HttpStatus.CREATED).body("Usuario creado exitosamente");
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al crear el usuario");
+            }
         } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al crear el usuario");
+            // El token no es válido, maneja el error adecuadamente
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token inválido");
         }
     }
 
