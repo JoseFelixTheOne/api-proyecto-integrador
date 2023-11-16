607eccb0dfabc08e5610fd5e5fa23d1c0b7a0444
Nemo <torrestjfelix@gmail.com>
2023-11-16 13:07:28 -0500

    Agregando validaciones y mensajes segun excepciones, el proyecto va mejorando. Uso de Validation, falta validar el endtime y coordinar bien como funcionara el bookedtime

--- src/main/java/com/cibertec/proyecto/integrador/entity/OrderDetailEntity.java
+++ src/main/java/com/cibertec/proyecto/integrador/entity/OrderDetailEntity.java
@@ -1,10 +1,16 @@
 package com.cibertec.proyecto.integrador.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
@@ -21,19 +27,23 @@ public class OrderDetailEntity {
     private BigDecimal subtotal;
     private BigDecimal price;
     private double days;
-    private int roomid;
+    @NotNull(message = "El campor 'roomid' no puede estar vacío")
+    private Integer roomid;
     @Column(name = "bookedtime")
     @Temporal(TemporalType.DATE)
-    private Date bookedtime;
+    private LocalDate bookedtime = LocalDate.now();
 
     @Column(name = "startedtime")
     @Temporal(TemporalType.DATE)
-    private Date startedtime;
+    @FutureOrPresent(message = "'startedtime' debe ser una fecha mayor o igual a la actual")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate startedtime;
 
     @Column(name = "endtime")
     @Temporal(TemporalType.DATE)
-    private Date endtime;
-    private Boolean deleted;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate endtime;
+    private Boolean deleted = false;
 
     @ManyToOne
     @JoinColumn(name = "orderid", insertable = false, updatable = false)
@@ -42,4 +52,9 @@ public class OrderDetailEntity {
     @ManyToOne
     @JoinColumn(name = "roomid", insertable = false, updatable = false)
     private RoomEntity room;
+
+    @AssertTrue(message = "La fecha 'endtime' debe ser mayor a 'startedtime'")
+    public boolean isValidEndtime() {
+        return endtime == null || endtime.isAfter(startedtime);
+    }
 }
