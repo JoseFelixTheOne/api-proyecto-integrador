76919120b57834cc4b62e37b414b63415ca8367d
Nemo <torrestjfelix@gmail.com>
2023-11-26 23:03:33 -0500

    quitando imports innecesarios

--- src/main/java/com/cibertec/proyecto/integrador/entity/Order.java
+++ src/main/java/com/cibertec/proyecto/integrador/entity/Order.java
@@ -1,10 +1,8 @@
 package com.cibertec.proyecto.integrador.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
